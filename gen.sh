bfe0144bdc90ad4732aa1e3bb73dd6f9e23cc6ab
Aidan Wyber <aidanwyber@gmail.com>
2026-06-16 16:40:40 +0200

    add automated template generator and implementor

---
+++ gen.sh
@@ -0,0 +1,5 @@
+
+read -p "Seed: " SEED
+python3 -m generator.template_generator --attempts 2000 --keep 10 --seed "$SEED"
+python3 -m generator.generate --template "random-10x17-$SEED"
+python3 generate.generate
